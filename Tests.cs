eed433d3cb22453eb7380612c01b989303ac6e65
Elizabete <deikoelizabete@gmail.com>
2026-03-27 19:59:05 +0200

    Ieviesta ebay meklēšanas automatizācija un tests

---
+++ Tests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace WindowsFormsApp1
+{
+    public class Tests
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.ebay.com");
+        }
+
+        [TearDown]
+        public void Close()
+        {
+            driver.Quit();
+        }
+
+        [Test]
+        public void Test1_field()
+        {
+            var element = driver.FindElement(By.Id("gh-ac"));
+            Assert.IsNotNull(element);
+        }
+
+        [Test]
+        public void Test2_search()
+        {
+            var element = driver.FindElement(By.Id("gh-btn"));
+            Assert.IsNotNull(element);
+        }
+    }
+}
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -7,14 +7,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        IWebDriver driver;
         public Form1()
         {
             InitializeComponent();
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.ebay.com");
+
+            System.Threading.Thread.Sleep(3000);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+            var searchBox = wait.Until(d => d.FindElement(By.Id("gh-ac")));
+            searchBox.Clear();
+            searchBox.SendKeys(textBox1.Text);
+
+            var searchBtn = wait.Until(d => d.FindElement(By.Id("gh-btn")));
+            searchBtn.Click();
+
+            wait.Until(d => d.Url.Contains("_nkw"));
+
+            string url = driver.Url;
+
+            textBox2.Text = url;
+            richTextBox1.AppendText(url + Environment.NewLine);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            driver.Navigate().Back();
+
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            driver.Quit();
         }
     }
 }
